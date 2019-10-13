5515d6138fe265f19e30e3efaae199cf6c4154d7
nzanoa <nzanoa@moyingugeek.com>
2019-10-13 13:47:38 +0200

    Add comments and fixing typos

--- BashScripting/ping_sweep.sh
+++ BashScripting/ping_sweep.sh
@@ -10,11 +10,14 @@ echo " CIDR and VLSM are not suported. By Nzanoa !!!"
 echo " eg. bash ping_sweep 192.168.4.0"
 echo
 
+# Create temp file
 echo $1 > .ping_sweep_ip
+# Use IP in temp file to extract Network part
 SUBNET=$(cat .ping_sweep_ip |cut -d "." -f 1,2,3)
 
 echo " Analyzing the ${SUBNET}.0/24 subdomain..."
 
+# Start pinging
 for BIT in {1..254}
 do
     ping -c 1 ${SUBNET}.${BIT} 2>&1 > /dev/null
@@ -25,6 +28,7 @@ do
     fi
 done
 
+# Remove temporary file
 rm -rf .ping_sweep_ip
 
 echo "[*] Done !!!"
